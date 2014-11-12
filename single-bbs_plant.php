9017d32f763d5c3b23cfd38c936764848e4d656b
Bob Albert <bob@superstan.com>
2014-11-11 17:55:43 -0800

    foo layout updates for single content pages

--- single-bbs_plant.php
+++ single-bbs_plant.php
@@ -1,28 +1,43 @@
 <?php get_header(); ?>
-<div class="row">
-	<div class="small-12 large-9 columns" role="main">
+<div class="row" xmlns="http://www.w3.org/1999/html">
+	<div class="small-12 large-10 columns" role="main">
 
 	<?php do_action('foundationPress_before_content'); ?>
 
 	<?php while (have_posts()) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<?php FoundationPress_entry_meta(); ?>
+				<h1 class="entry-title"><?php //the_title(); ?></h1>
+				<?php //FoundationPress_entry_meta(); ?>
 			</header>
 			<?php do_action('foundationPress_post_before_entry_content'); ?>
+
 			<div class="entry-content">
 
-			<?php if ( has_post_thumbnail() ): ?>
-				<div class="row">
-					<div class="column">
-						<?php the_post_thumbnail('', array('class' => 'th')); ?>
-					</div>
-				</div>
-			<?php endif; ?>
+				<?php if ( has_post_thumbnail() ): ?>
+					<div class="row">
+						<div class="large-7 columns">
+							<?php the_post_thumbnail('', array('class' => 'th')); ?>
 
-			<?php the_content(); ?>
+						</div>
+						<div class="large-5 columns">
+							<h3><?php echo get_post_meta( get_the_ID(), 'Botanical Name', true ); ?></h3>
+							<h4><?php echo get_post_meta( get_the_ID(), 'Common Name', true ); ?></h4>
+
+							<p><a href="#" >Add to favorites</a></p>
+							<p><a href="#">Add to shopping list</a></p>
+						</div>
+					</div>
+				<?php endif; ?>
+
+				<div class="row column">
+					<?php the_content(); ?>
+				</div>
+
+
+
 			</div>
+
 			<footer>
 				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
 				<p><?php the_tags(); ?></p>
@@ -36,6 +51,6 @@
 	<?php do_action('foundationPress_after_content'); ?>
 
 	</div>
-	<?php get_sidebar(); ?>
+	<?php get_sidebar('plant'); ?>
 </div>
 <?php get_footer(); ?>
--- single-bbs_recipe.php
+++ single-bbs_recipe.php
@@ -1,28 +1,42 @@
 <?php get_header(); ?>
 <div class="row">
-	<div class="small-12 large-9 columns" role="main">
+	<div class="small-12 large-10 columns" role="main">
 
 	<?php do_action('foundationPress_before_content'); ?>
 
 	<?php while (have_posts()) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<?php FoundationPress_entry_meta(); ?>
+				<h1 class="entry-title"><?php //the_title(); ?></h1>
+				<?php //FoundationPress_entry_meta(); ?>
 			</header>
 			<?php do_action('foundationPress_post_before_entry_content'); ?>
+
 			<div class="entry-content">
 
-			<?php if ( has_post_thumbnail() ): ?>
-				<div class="row">
-					<div class="column">
-						<?php the_post_thumbnail('', array('class' => 'th')); ?>
-					</div>
-				</div>
-			<?php endif; ?>
+				<?php if ( has_post_thumbnail() ): ?>
+					<div class="row">
+						<div class="large-7 columns">
+							<?php the_post_thumbnail('', array('class' => 'th')); ?>
 
-			<?php the_content(); ?>
+						</div>
+						<div class="large-5 columns">
+							<h3><?php the_title(); ?></h3>
+							<p><a href="#">Add to favorites</a></p>
+							<p><a href="#">Add to shopping list</a></p>
+							<p><strong>Why We Like This Recipe:</strong><br/><?php echo get_post_meta( get_the_ID(), 'Why We Like', true ); ?></p>
+						</div>
+					</div>
+				<?php endif; ?>
+
+				<div class="row column">
+					<?php the_content(); ?>
+				</div>
+
+
+
 			</div>
+
 			<footer>
 				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
 				<p><?php the_tags(); ?></p>
